0e3d6a849ce5ea0c2b69469567df811e62dc4b08
ntysean <ntysean@gmail.com>
2021-04-01 13:53:24 +0800

    Add files via upload

---
+++ prac_02/randoms.py
@@ -0,0 +1,10 @@
+
+
+import random
+print(random.randint(1, 3))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+### 1) largest number is 20, smallest 5. 2)3, 9. no. 3) 2.5, 5.4
+
+print(random.randint(1,100))
