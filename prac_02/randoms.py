6c80931ad5916d6ab525e3ed5951ec40002eda19
ntysean <ntysean@gmail.com>
2021-04-01 20:49:18 +0800

    Update randoms.py

--- prac_02/randoms.py
+++ prac_02/randoms.py
@@ -1,10 +1,12 @@
 
 
 import random
-print(random.randint(1, 3))  # line 1
+print(random.randint(5, 20))  # line 1
 print(random.randrange(3, 10, 2))  # line 2
 print(random.uniform(2.5, 5.5))  # line 3
 
-### 1) largest number is 20, smallest 5. 2)3, 9. no. 3) 2.5, 5.4
+### 1) largest number is 20, smallest 5. 
+### 2)smallest number is 3, largest number is 9. no it could not have produced a 4. 
+### 3) 2.5, 5.4
 
 print(random.randint(1,100))
