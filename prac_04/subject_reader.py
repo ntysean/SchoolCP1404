c00a844b17e7cfb791b02177b98f4bdb8971c943
ntysean <ntysean@gmail.com>
2021-04-13 17:02:14 +0800

    Update subject_reader.py

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -13,12 +13,12 @@ def get_data():
     data = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        print(line) 
+        print(repr(line))  
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
         print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        parts[2] = int(parts[2]) 
         print(parts)  # See if that worked
         data.append(parts)
     input_file.close()
