b6280b25944ae81304b295cb52d315d2556d77c9
ntysean <ntysean@gmail.com>
2021-04-13 17:09:13 +0800

    Update list_exercises.py

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,5 +1,4 @@
 
-# 1. Numbers stuff
 numbers = []
 for i in range(5):
     number = int(input("Number: "))
@@ -11,7 +10,6 @@ print("The smallest number is", min(numbers))
 print("The largest number is", max(numbers))
 print("The average of the numbers is", sum(numbers) / len(numbers))
 
-# 2. Woefully inadequate security checker...
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye',
              'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command',
              'ExecState', 'InteractiveConsole', 'InterpreterInterface',
